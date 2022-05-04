8fa9caf78d787adae5f90a8c53245b2af14aa08c
Jakob Holkestad Molnes <jakob@dirik.no>
2022-05-05 01:28:22 +0200

    add comments and remove console logs

--- content_scripts/buttonClick.js
+++ content_scripts/buttonClick.js
@@ -2,7 +2,12 @@ var cmpoints = document.getElementsByClassName("community-points-summary")[0];
 var buttons = cmpoints.getElementsByTagName("button");
 
 
-
+/**
+ * check if there are 2 elements in the array
+ *      if there are 2 elements, return true
+ * @param {Array} buttons 
+ * @returns boolean
+ */
 function checkIf2Buttons(buttons) {
     if (buttons.length == 2) {
         return true;
@@ -10,7 +15,10 @@ function checkIf2Buttons(buttons) {
     return false;
 }
 
-
+/**
+ * simply click the button
+ * @param {button} button 
+ */
 function clickButton(button) {
     button.click();
 }
@@ -19,32 +27,30 @@ function onError(error) {
     console.log(`Error: ${error}`);
 }
 
+/**
+ * Function to run after the value is checked
+ *    if the value is true, click the button
+ *    if the value is false, do nothing
+ * @param {String} item 
+ */
 function onGot(item) {
     let toggle = false;
     if (item.checked) {
         toggle = item.checked;
     }
-    var interval = null
     if (toggle) {
-
-
         if (checkIf2Buttons(buttons)) {
             clickButton(buttons[1]);
         }
-        console.log("clicked");
-    }
-    else {
-        clearInterval(interval);
-        console.log("did not click");
-
     }
 
 }
+
 setInterval(function () {
     let getting = browser.storage.sync.get("checked");
 
     getting.then(onGot, onError);
-}, 5000);
+}, 10000);
 
 
 
